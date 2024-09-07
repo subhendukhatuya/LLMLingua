906aa830d7b536f0da6c15c39aa502ed9e712b6d
Mangalik Mitra <mitramango@gmail.com>
2024-09-08 02:49:27 +0530

    track time taken llmlingua 1 vs 2

--- test_setup.py
+++ test_setup.py
@@ -1,4 +1,5 @@
 from llmlingua import PromptCompressor
+import time
 
 prompt_complex = open("./prompt_hardest.txt").read()
 contxt = prompt_complex.split('\n\n')
@@ -15,13 +16,19 @@ dict_list = []
 for prompt in contxt:
     
     i += 1
+    st1 = time.time()
     compressed_prompt = llm_lingua.compress_prompt(prompt,)
+    st2 = time.time()
     compressed_prompt2 = llm_lingua2.compress_prompt(prompt,)
-    
+    t2 = time.time() - st2
+    t1 = st2-st1
+     
     mydict = dict()
     mydict['Number'] = i
     mydict['LLMLingua Rate'] = compressed_prompt['rate']
     mydict['LLMLingua2 Rate'] = compressed_prompt2['rate']
+    mydict['LLMLingua Time'] = t1
+    mydict['LLMLingua2 Time'] = t2
     mydict['LLMLingua Compressed Prompt'] = compressed_prompt['compressed_prompt']
     mydict['LLMLingua2 Compressed Prompt'] = compressed_prompt2['compressed_prompt']
 
