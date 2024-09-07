8e750f1db9906138e24f119029fe7240c712baef
Mangalik Mitra <mitramango@gmail.com>
2024-09-08 00:46:40 +0530

    changed the example (why is llmlingua1 not working?)

--- test_setup.py
+++ test_setup.py
@@ -1,17 +1,24 @@
 from llmlingua import PromptCompressor
 
-contxt = '''Q: I have a blackberry, a clarinet, a nectarine, a plum, a strawberry, a banana, a flute, an orange, and a violin. How many
-fruits do I have?
-A: Let’s think step by step.
-We first identify the fruits on the list and include their quantity in parentheses:
-- blackberry (1) - nectarine (1) - plum (1) - strawberry (1) - banana (1) - orange (1)
-Now, let’s add the numbers in parentheses: 1 + 1 + 1 + 1 + 1 + 1 = 6. So the answer is 6.'''
+contxt = '''Question: Sam bought a dozen boxes, each with 30 highlighter pens inside, for $10 each box. He rearranged five of these
+boxes into packages of six highlighters each and sold them for $3 per package. He sold the rest of the highlighters separately
+at the rate of three pens for $2. How much profit did he make in total, in dollars?
+Let’s think step by step
+Sam bought 12 boxes x $10 = $120 worth of highlighters.
+He bought 12 * 30 = 360 highlighters in total.
+Sam then took 5 boxes × 6 highlighters/box = 30 highlighters.
+He sold these boxes for 5 * $3 = $15
+After selling these 5 boxes there were 360 - 30 = 330 highlighters remaining.
+These form 330 / 3 = 110 groups of three pens.
+He sold each of these groups for $2 each, so made 110 * 2 = $220 from them.
+In total, then, he earned $220 + $15 = $235.
+Since his original cost was $120, he earned $235 - $120 = $115 in profit.
+The answer is 115'''
 
 llm_lingua = PromptCompressor()
 
 compressed_prompt = llm_lingua.compress_prompt(
-    [contxt],
-    use_context_level_filter=True,
+    contxt,
     target_token=60
 )
 
