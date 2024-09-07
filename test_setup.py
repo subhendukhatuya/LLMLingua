5d7398b783087456eeb228ec47ce2cb4868c43af
Mangalik Mitra <mitramango@gmail.com>
2024-09-08 01:51:26 +0530

    storing the results of comparison between llmlingua 1v2

--- test_setup.py
+++ test_setup.py
@@ -10,20 +10,26 @@ llm_lingua2 = PromptCompressor(
     use_llmlingua2=True, # Whether to use llmlingua-2
 )
 
+i = 0
+dict_list = []
 for prompt in contxt:
-    compressed_prompt = llm_lingua.compress_prompt(
-        prompt,
-    )
-    compressed_prompt2 = llm_lingua2.compress_prompt(
-        prompt,
-    )
-    print(prompt)
-    print()
-    print("LLMLingua1")
-    print(compressed_prompt)
-    print()
-    print("LLMLingua2")
-    print(compressed_prompt2)
+    
+    i += 1
+    compressed_prompt = llm_lingua.compress_prompt(prompt,)
+    compressed_prompt2 = llm_lingua2.compress_prompt(prompt,)
+    
+    mydict = dict()
+    mydict['Number'] = i
+    mydict['LLMLingua Rate'] = compressed_prompt['rate']
+    mydict['LLMLingua2 Rate'] = compressed_prompt2['rate']
+    mydict['LLMLingua Compressed Prompt'] = compressed_prompt['compressed_prompt']
+    mydict['LLMLingua2 Compressed Prompt'] = compressed_prompt2['compressed_prompt']
 
+    dict_list.append(mydict)
 
+import csv
 
+with open('compare_llmlingua_1v2.csv', 'w', encoding='utf8', newline='') as output_file:
+    fc = csv.DictWriter(output_file, fieldnames=dict_list[0].keys(),)
+    fc.writeheader()
+    fc.writerows(dict_list)
